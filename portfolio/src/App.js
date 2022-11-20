0d80776e1ee69eca15637c41bc40911b6b9451d7
Stephen Novelli <snovelli1021@gmail.com>
2022-11-20 14:56:53 -0800

    Setup components and imports, outline a working Navbar.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar/Navbar.js";
+// import Header from "./components/Header/Header.js"
+// import Projects from "./components/Projects/Projects.js ";
+//import Footer from "./components/Footer/Footer.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {/* <Header />
+      <Projects />
+      <Footer />  */}
     </div>
   );
 }
