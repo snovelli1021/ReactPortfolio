3e01757464f530d732b087f795bd795a5a689cb5
Stephen Novelli <snovelli1021@gmail.com>
2022-11-30 21:05:06 -0800

    Created outline for form on Contact Page. Needs styled and js.

--- portfolio/src/components/pages/Contact/Contact.js
+++ portfolio/src/components/pages/Contact/Contact.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { CForm, CFormLabel, CFormInput, CFormText } from "@coreui/react";
 
+//Write function to submit form and style page
 const Contact = () => {
   return (
     <div>
       <h1>Contact Me</h1>
+      <CForm>
+        <CFormLabel htmlFor="exampleFormControlInput1">
+          Please enter your information below
+        </CFormLabel>
+        <CFormInput
+          type="name"
+          id="nameInput"
+          placeholder="First and Last Name"
+          aria-describedby="exampleFormControlInputHelpInline"
+        />
+        <CFormInput
+          type="email"
+          id="emailInput"
+          placeholder="emailaddress@example.com"
+          aria-describedby="exampleFormControlInputHelpInline"
+        />
+        <CFormInput
+          type="Message"
+          id="messageInput"
+          placeholder="Please leave a detailed message"
+          aria-describedby="exampleFormControlInputHelpInline"
+        />
+
+        <CFormText component="span" id="exampleFormControlInputHelpInline">
+          Must be less than 200 characters.
+        </CFormText>
+        <button id="submitBtn">Submit!</button>
+      </CForm>
     </div>
   );
 };
