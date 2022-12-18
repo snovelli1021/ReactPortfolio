312a3fda2c8b7c3a19439fd43f09df9aea63dea3
Stephen Novelli <snovelli1021@gmail.com>
2022-12-17 21:54:15 -0800

    Refactored footer as a react component, and styled page some.

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -1,69 +1,34 @@
 import React from "react";
-import { MDBFooter, MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
 import "./Footer.css";
+import data from "./footerData.json";
 
 export default function Footer() {
   return (
-    <MDBFooter
-      className="bg-secondary text-white text-center text-md-start"
-      id="footerCustom"
-    >
-      <MDBContainer className="p-4">
-        <MDBRow>
-          <MDBCol>
-            <h5 className="text-uppercase">Contact:</h5>
-
-            <p>snovelli92@msn.com</p>
-          </MDBCol>
-
-          <MDBCol lg="4" md="4" className="mb-4 mb-md-0">
-            <h5 className="text-uppercase.mb-0">Links:</h5>
-            <ul className="list-unstyled">
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/stephen-novelli-a4b97b242/"
-                  alt="Link to LinkedIn Profile"
-                  target="blank"
-                  className="text-white"
-                >
-                  LinkedIn
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/snovelli1021"
-                  alt="Link to GitHub Profile"
-                  target="blank"
-                  className="text-white"
-                >
-                  GitHub
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="https://www.arcgis.com/home/user.html?user=SNovelli"
-                  alt="Link to ArcGIS Online Profile"
-                  target="blank"
-                  className="text-white"
-                >
-                  ArcGIS Online
-                </a>
-              </li>
-            </ul>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-
+    <div className="container-fluidFooter">
+      <div className="contact">
+        <h5>Contact:</h5>
+      </div>
+      {data.map(({ url, profile }) => (
+        <div className="footerBlock">
+          <ul className="profileLinks">
+            <li>
+              <a href={url} alt="Link to online profile" target="blank">
+                {profile}
+              </a>
+            </li>
+          </ul>
+        </div>
+      ))}
       <div
-        className="text-center p-3"
+        className="text-center"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
         © 2022 Copyright:
-        <a> Stephen Novelli</a>
+        <p> Stephen Novelli</p>
         <br></br>
         {/* Background Image Source: Adam San, "BLACK AND GOLD HOT POWERPOINT BACKGROUND", SLIDEBACKGROUND.com, Feb 18, 2021, https://www.slidebackground.com/free-image/black-and-gold-hot-powerpoint-background-5112.html */}
         <a
+          className="imageText"
           id="imageSrc"
           href="https://www.slidebackground.com/free-image/black-and-gold-hot-powerpoint-background-5112.html"
           target="blank"
@@ -71,6 +36,6 @@ export default function Footer() {
           Background Image:
         </a>
       </div>
-    </MDBFooter>
+    </div>
   );
 }
--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar.js";
 import "./Header.css";
 
